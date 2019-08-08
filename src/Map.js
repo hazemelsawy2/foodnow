4ab6e17abcb0f12d04498f9d5d1237c82fa5e620
Hazem Elsawy <53141407+hazemelsawy@users.noreply.github.com>
2019-08-08 19:32:18 +0100

    Update Map.js

--- src/Map.js
+++ src/Map.js
@@ -152,7 +152,7 @@ const WrappedMap = withScriptjs(withGoogleMap(Map));
 export default function App() {
     return (<div style={{ width: '100vw', height: '100vh' }}>
         <WrappedMap
-            googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyASm5cBnhfHyTc4GZQhoOBrC_8Otsozsp0`}
+            googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=`}
             loadingElement={<div style={{ height: `100%` }} />}
             containerElement={<div style={{ height: `100%` }} />}
             mapElement={<div style={{ height: `100%` }} />}
