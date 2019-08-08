a708e06dc1636b09bb75050e1138bd061630ec0d
hazemelsawy <hazemelsawy@hazems-mbp-2.home>
2019-08-08 21:30:05 +0100

    Updates

---
+++ precache-manifest.fbbf89f2d5a17b2d0c6f7612a9550fea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "822018141ab05604f380a24f5416d7e7",
+    "url": "/foodnow/index.html"
+  },
+  {
+    "revision": "b7a1c5a85c5508ed4e23",
+    "url": "/foodnow/static/css/main.2735ea30.chunk.css"
+  },
+  {
+    "revision": "3e0c878b1442c5dd57e9",
+    "url": "/foodnow/static/js/2.b857776b.chunk.js"
+  },
+  {
+    "revision": "b7a1c5a85c5508ed4e23",
+    "url": "/foodnow/static/js/main.e6231990.chunk.js"
+  },
+  {
+    "revision": "e79810581ad93208168d",
+    "url": "/foodnow/static/js/runtime~main.42b81072.js"
+  },
+  {
+    "revision": "8b16badeea503c4897e6e7530d6e8635",
+    "url": "/foodnow/static/media/go.8b16bade.svg"
+  },
+  {
+    "revision": "7f12a92b83cdd13164131eb7cad1d4ae",
+    "url": "/foodnow/static/media/mapRestaurant.7f12a92b.png"
+  },
+  {
+    "revision": "2a54bf64cf8e1b02dff9c85a37d94282",
+    "url": "/foodnow/static/media/restaurant.2a54bf64.jpg"
+  }
+]);
